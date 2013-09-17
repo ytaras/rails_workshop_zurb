5f601753d7b9770d35affb00cfdf9372ee9a48d6
Yura Taras <yura.taras@gmail.com>
2013-09-18 01:10:15 +0300

    Fixed n+1 selects problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.with_users
   end
 
   # GET /posts/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, :body, presence: true
   belongs_to :user
+
+  scope :with_users, ->{ includes(:user) }
 end
